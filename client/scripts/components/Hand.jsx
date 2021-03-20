0ba6d9e0a0c4e368b5bd55b2147d364971f83db8
Will Allan <will.allan@disneystreaming.com>
2021-03-19 22:06:09 -0700

    Card animations

--- client/scripts/components/Hand.jsx
+++ client/scripts/components/Hand.jsx
@@ -3,15 +3,20 @@ import PropTypes from 'prop-types';
 import { TransitionGroup, CSSTransition } from 'react-transition-group';
 import Card from './Card.jsx';
 
+const timeouts = {
+  enter: 1000,
+  exit: 1000
+};
+
 const Hand = (props) => {
   console.log('Hand render', props);
 
   return (
-    <TransitionGroup className='hand'>
+    <TransitionGroup className="hand">
       {props.hand.cards.map((card, index) => {
-        const cardRef = createRef(null);
+        const cardRef = createRef(null); // avoids findDOMNode warning
         return (
-          <CSSTransition key={index} nodeRef={cardRef} timeout={2500}>
+          <CSSTransition key={index} nodeRef={cardRef} timeout={timeouts}>
             <Card ref={cardRef} card={card} />
           </CSSTransition>
         );
