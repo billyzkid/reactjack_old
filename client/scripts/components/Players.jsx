dde232ddbb59dc4efcae2c985402f4d32fcce81f
Will Allan <will.allan@disneystreaming.com>
2021-03-23 13:22:23 -0700

    Seat players

--- client/scripts/components/Players.jsx
+++ client/scripts/components/Players.jsx
@@ -7,10 +7,11 @@ const Players = (props) => {
   console.log('Players render', props);
 
   const { players } = useStateContext();
+  const seatedPlayers = sitPlayers(players);
 
   return (
     <div className="players">
-      {players.map((player, index) => (
+      {seatedPlayers.map((player, index) => (
         <Player key={index} player={player} />
       ))}
     </div>
@@ -20,5 +21,48 @@ const Players = (props) => {
 // Players.propTypes = {
 //   foo: PropTypes.bool.isRequired
 // };
+
+const dummyPlayer = { dummy: true };
+
+function sitPlayers(players) {
+  const seatedPlayers = [];
+  const primaryPlayer = players.find((player) => player.primary);
+
+  if (primaryPlayer) {
+    const otherPlayers = players.filter((player) => player !== primaryPlayer);
+
+    if (otherPlayers.length === 0) {
+      seatedPlayers.push(primaryPlayer);
+    } else if (otherPlayers.length === 1) {
+      seatedPlayers.push(otherPlayers[0], primaryPlayer, dummyPlayer);
+    } else if (otherPlayers.length === 2) {
+      seatedPlayers.push(otherPlayers[0], primaryPlayer, otherPlayers[1]);
+    } else if (otherPlayers.length === 3) {
+      seatedPlayers.push(otherPlayers[2], otherPlayers[0], primaryPlayer, otherPlayers[1], dummyPlayer);
+    } else if (otherPlayers.length === 4) {
+      seatedPlayers.push(otherPlayers[2], otherPlayers[0], primaryPlayer, otherPlayers[1], otherPlayers[3]);
+    } else {
+      throw new Error('table seating is limited to 5 players');
+    }
+  } else {
+    if (players.length === 0) {
+      console.warn('no players to seat');
+    } else if (players.length === 1) {
+      seatedPlayers.push(players[0]);
+    } else if (players.length === 2) {
+      seatedPlayers.push(players[1], players[0], dummyPlayer);
+    } else if (players.length === 3) {
+      seatedPlayers.push(players[1], players[0], players[2]);
+    } else if (players.length === 4) {
+      seatedPlayers.push(players[3], players[1], players[0], players[2], dummyPlayer);
+    } else if (players.length === 5) {
+      seatedPlayers.push(players[3], players[1], players[0], players[2], players[4]);
+    } else {
+      throw new Error('table seating is limited to 5 players');
+    }
+  }
+
+  return seatedPlayers;
+}
 
 export default Players;
